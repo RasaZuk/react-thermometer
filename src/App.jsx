f1b340f9890787aae386aef58cdfab76f7805762
Rasa <rasa.zukauskaite@gmail.com>
2024-09-06 14:02:36 +0300

    Innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { MainDiv } from "./components/MainDiv.jsx";
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Funny thermometer</h1>
+   
+   <div>
+    <MainDiv />
+   </div>
+   
+
+
+    </>
+   
+  )
+}
+
+export default App
---
+++ src/components/MainDiv.jsx
@@ -0,0 +1,12 @@
+
+
+export function MainDiv() {
+    return (
+        <div className="container">
+            <div className="thermo">
+                <h2>0 </h2>
+            </div>
+        </div>
+            
+    );
+}
